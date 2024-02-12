bdf5e3a6361d587fa64f0f633ed4f0050566d984
fadwafa02 <fadad2002@gmail.com>
2024-02-12 13:47:16 +0100

    modif4

--- src/app/calender-medecin/calender-medecin.module.ts
+++ src/app/calender-medecin/calender-medecin.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+
 
 import { IonicModule } from '@ionic/angular';
 
@@ -8,12 +8,24 @@ import { CalenderMedecinPageRoutingModule } from './calender-medecin-routing.mod
 
 import { CalenderMedecinPage } from './calender-medecin.page';
 
+
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+
+
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
+
     IonicModule,
-    CalenderMedecinPageRoutingModule
+    CalenderMedecinPageRoutingModule,
+    NgbModalModule,
+   
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    }),
   ],
   declarations: [CalenderMedecinPage]
 })
